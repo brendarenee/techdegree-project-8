d2287b85455bba53e48882ebb4d19497bc5f9693
Brenda Butler <brendabutler@Horbut-Family-iMac.local>
2020-11-11 19:44:42 -0800

    Clean up sass and add JS functionality to add abbreviated state

--- script.js
+++ script.js
@@ -10,6 +10,82 @@ const searchName = document.querySelector(".search-input");
 const arrowRight = document.querySelector(".arrow-right");
 const arrowLeft = document.querySelector(".arrow-left");
 
+
+/************************
+ HELPER FUNCTIONS
+************************/
+
+function stateAbbreviation(input) {
+  var states = [
+    ['Alabama', 'AL'],
+    ['Alaska', 'AK'],
+    ['American Samoa', 'AS'],
+    ['Arizona', 'AZ'],
+    ['Arkansas', 'AR'],
+    ['Armed Forces Americas', 'AA'],
+    ['Armed Forces Europe', 'AE'],
+    ['Armed Forces Pacific', 'AP'],
+    ['California', 'CA'],
+    ['Colorado', 'CO'],
+    ['Connecticut', 'CT'],
+    ['Delaware', 'DE'],
+    ['District Of Columbia', 'DC'],
+    ['Florida', 'FL'],
+    ['Georgia', 'GA'],
+    ['Guam', 'GU'],
+    ['Hawaii', 'HI'],
+    ['Idaho', 'ID'],
+    ['Illinois', 'IL'],
+    ['Indiana', 'IN'],
+    ['Iowa', 'IA'],
+    ['Kansas', 'KS'],
+    ['Kentucky', 'KY'],
+    ['Louisiana', 'LA'],
+    ['Maine', 'ME'],
+    ['Marshall Islands', 'MH'],
+    ['Maryland', 'MD'],
+    ['Massachusetts', 'MA'],
+    ['Michigan', 'MI'],
+    ['Minnesota', 'MN'],
+    ['Mississippi', 'MS'],
+    ['Missouri', 'MO'],
+    ['Montana', 'MT'],
+    ['Nebraska', 'NE'],
+    ['Nevada', 'NV'],
+    ['New Hampshire', 'NH'],
+    ['New Jersey', 'NJ'],
+    ['New Mexico', 'NM'],
+    ['New York', 'NY'],
+    ['North Carolina', 'NC'],
+    ['North Dakota', 'ND'],
+    ['Northern Mariana Islands', 'NP'],
+    ['Ohio', 'OH'],
+    ['Oklahoma', 'OK'],
+    ['Oregon', 'OR'],
+    ['Pennsylvania', 'PA'],
+    ['Puerto Rico', 'PR'],
+    ['Rhode Island', 'RI'],
+    ['South Carolina', 'SC'],
+    ['South Dakota', 'SD'],
+    ['Tennessee', 'TN'],
+    ['Texas', 'TX'],
+    ['US Virgin Islands', 'VI'],
+    ['Utah', 'UT'],
+    ['Vermont', 'VT'],
+    ['Virginia', 'VA'],
+    ['Washington', 'WA'],
+    ['West Virginia', 'WV'],
+    ['Wisconsin', 'WI'],
+    ['Wyoming', 'WY'],
+  ];
+
+  for (let i=0; i<states.length; i++) {
+    if (states[i][0] === input) {
+      return (states[i][1]);
+    }
+  }
+}
+
 /************************
  DISPLAY FUNCTIONS
 ************************/
@@ -45,6 +121,7 @@ function displayEmployees(employeeData) {
 function displayModal (index) {
   let { name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
   let date = new Date(dob.date);
+  let st = stateAbbreviation(state);
 
   const modalHTML= `
     <img class="avatar" src="${picture.large}" alt="">
@@ -55,6 +132,7 @@ function displayModal (index) {
       <hr>
       <p class="phone">${phone}</p>
       <p class="address">${street.number} ${street.name}</p>
+      <p class="address">${city}, ${st}&nbsp;&nbsp;${postcode}</p>
       <p class="birthdate">${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
   `
